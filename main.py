6456bd6f72d39bca5f61f8af85298327347d4f52
electragn <ilektra.giannopoulou@ucdconnect.ie>
2024-06-29 12:44:14 +0300

    We add the deck and card classes defined in the deck.py file because we need to have all the classes in one common file.

--- main.py
+++ main.py
@@ -1,3 +1,48 @@
+import random
+
+#Card Class: define card ranks and suits
+class Card:
+    #represents a single card, defined by its suit and rank
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    #returns a readable string representation of the card
+    def __repr__(self):
+        return f'{self.rank} of {self.suit}'
+
+
+#Deck Class: set up the deck, shuffle it, and handle card draws
+class Deck:
+    #represents a deck of cards to be used in the game
+    suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+
+    #initialises the deck by creating a full set of 52 cards and then shuffling them
+    def __init__(self):
+        self.cards = []
+        for suit in self.suits:
+            for rank in self.ranks:
+                self.cards.append(Card(suit, rank))
+        self.shuffle()
+
+    #shuffles the deck to randomise the order of the cards
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    #draws the top card from the deck and returs it
+    def draw_card(self):
+        if not self.cards:
+            #reinitialises the deck and shuffles again if out of cards
+            self.__init__()  
+            print("Deck was empty. Reshuffled.")
+        return self.cards.pop()
+
+    #returns a string representation showing the number of cards left in the deck
+    def __repr__(self):
+        return f'Deck of {len(self.cards)} cards'
+    
+    
 #Player Class: Blackjack player
 class Player:
     
